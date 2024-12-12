516ca4a0897fde15dc9722c761ae5a2e679f4937
erwann <erwann.robinlesne@gmail.com>
2024-12-12 12:33:31 +0100

    Finalisation du backend
    Fichiers modifiés :
        - src/Controller/CineController.php
        - templates/cine/index.html.twig
        - templates/cine/search.html.twig

--- src/Controller/CineController.php
+++ src/Controller/CineController.php
@@ -9,13 +9,13 @@ use Symfony\Component\Serializer\Encoder\JsonDecode;
 
 class CineController extends AbstractController
 {
-    #[Route('/cine', name: 'app_movie')]
+    const themoviedbApiKey = "cc14ac256dfbb6167";
+
+    #[Route('/', name: 'app_movie')]
     public function index(): Response
     {
-        $themoviedbApiKey = "cc14ac256dfbb6167594a212f1594f1e";
-
         // Création du endpoint de l'API (film recherché + clé API)
-        $endPoint = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&api_key=' . $themoviedbApiKey . '&language=fr-FR';
+        $endPoint = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&api_key=' . self::themoviedbApiKey . '&language=fr-FR';
 
         $tmdbImagePrefix = "https://media.themoviedb.org/t/p/w300_and_h450_bestv2";
 
@@ -38,13 +38,11 @@ class CineController extends AbstractController
         ]);
     }
 
-    #[Route('/cine/search', name: 'app_search')]
-    public function search(): Response
+    #[Route('/{search}', name: 'app_search')]
+    public function search(int $search): Response
     {
-        $themoviedbApiKey = "cc14ac256dfbb6167594a212f1594f1e";
-
         // Création du endpoint de l'API (film recherché + clé API)
-        $endPoint = 'https://api.themoviedb.org/3/search/movie?query=&api_key=' . $themoviedbApiKey . '&language=fr-FR';
+        $endPoint = 'https://api.themoviedb.org/3/movie/' . $search . '?api_key=' . self::themoviedbApiKey . '&language=fr-FR';
 
         $tmdbImagePrefix = "https://media.themoviedb.org/t/p/w300_and_h450_bestv2";
 
@@ -63,7 +61,8 @@ class CineController extends AbstractController
         return $this->render('cine/search.html.twig', [
             // On envoie au TWIG uniquement le contenu de Search
             'movies' => $json,
-            'ImagePrefix' => $tmdbImagePrefix
+            'ImagePrefix' => $tmdbImagePrefix,
+            'movieSearch' => $search
         ]);
     }
 }
